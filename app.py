81eda3774cb0e88060a26055e0f4b174f5ddeafe
Anu1495 <141367947+Anu1495@users.noreply.github.com>
2025-01-20 13:32:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.H1("Embedded Power BI Dashboard"),
     html.Iframe(
-        src="https://app.powerbi.com/view?r=eyJrIjoiY2NkZTc0YjctNmM3ZC00NGE1LTgzZmYtZTJlMzg0ZjdlOTQzIiwidCI6ImM1MmRiMzE5LTk5ZDgtNGFjMi1hNTBmLTU4ZjdhMDg3N2M0NyJ9&pageName=ReportSection97554cd968fdad03e640",
+        src="https://app.powerbi.com/view?r=eyJrIjoiMTQzOTExMmItZjIxMS00MmY4LWFjYjMtNGUzNmE2ODE0YmFjIiwidCI6ImM1MmRiMzE5LTk5ZDgtNGFjMi1hNTBmLTU4ZjdhMDg3N2M0NyJ9",
         style={"width": "100%", "height": "1000px", "border": "none"}
     )
 ])
